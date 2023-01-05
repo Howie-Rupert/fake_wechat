303ba01cc76786bfe373eecebeb01edc70e7457e
wangqiannan <861873232@qq.com>
2023-01-05 18:23:35 +0800

    登录/注册页面完成

---
+++ src/views/MainPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div @click="back_pg">mainpage</div>
+</template>
+
+<script>
+const { ipcRenderer } = window.require("electron");
+export default {
+    mounted(){
+        ipcRenderer.send('LoginSuccess');
+    },
+    methods:{
+        back_pg(){
+            this.$router.push('/')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
